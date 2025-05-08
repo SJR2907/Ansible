674494656d9969e6a8350b4dde105bdb2b2f04a3
SJR2907 <sjtacademy32@gmail.com>
2025-05-08 14:40:40 +0530

    variable-arguments

---
+++ 04-variable.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+Person1=$1
+Person2=$2
+echo "$Person1:: Hi $Person2, How are you?"
+echo "$Person2:: Hello $Person1. I am fine, How are you doing?"
+echo "$Person1:: I am doing good $Person2. what's going on?"
+echo "$Person2:: I started learning shell script $Person1"

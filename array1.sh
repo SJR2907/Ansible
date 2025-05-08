e4bbc441d059481172938ee5290b12e27d620063
SJR2907 <sjtacademy32@gmail.com>
2025-05-08 18:34:00 +0530

    arrayfruitscript

---
+++ array1.sh
@@ -0,0 +1,9 @@
+#!/bin/bash 
+
+FRUITS=("arun" "ramya" "mom") # Array 
+
+echo "First fruit:${FRUITS[0]}"
+echo "Second fruit: ${FRUITS[1]}"
+echo "Third fruit: ${FRUITS[2]}"
+
+echo "print all fruits: ${FRUIT[@]}"

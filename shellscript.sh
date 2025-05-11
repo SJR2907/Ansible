a45581ec5c5744e7fdc7260e8e9d2c8d9de39690
SJR2907 <sjtacademy32@gmail.com>
2025-05-11 11:32:14 +0530

    shellintroduction

---
+++ shellscript.sh
@@ -0,0 +1,40 @@
+what is shell?
+shell is responsible to read commands/applications provided by user.
+shell will check whether command is valid or not and whether it is properly used or not. if everything is proper then shell interpretes converts that command into kernel.
+understandle form that interpreted command will be handover to kernel 
+kernel is responsible to execute that command with the help of hardware. (cpu, ram, processor, hardisk)
+shell acts as interface between user and kernel.
+shell+kernel is nothing but operating system 
+
+Types of Shells:
+
+There are multiple types of shells are available.
+1)Bourne shell:
+It is developed by stephen bourne
+This is the first shell which is developed for unix flavors (solaris)
+By using sh command we can access this shell.
+2) Bash shell:
+Bash -> Bourne shell 
+it is advanced version of bourne shell 
+This is the default shell for most of the linux flavours 
+3) korn shell: 
+it is developed by David korn 
+Mostly this shell used in IBM Aix operating system 
+By using ksh command we can access this shell 
+4) T shell: 
+T means Terminal 
+it is advanced version of cshell:
+This is most commonly used shell in Hp unix 
+by using tcsh command we can access this shell 
+
+5)Cshell:
+Developed by Bill joy 
+c meant for california university
+it is also by default available with unix 
+by using csh command we can access this shell 
+
+6) Zshell:
+ developed by Paul 
+ By using zsh command we can access this shell 
+
+

85a80a41b124089d6a417735b95475aab79fa97a
SJR2907 <sjtacademy32@gmail.com>
2025-05-14 11:33:28 +0530

    pingrequestcd

--- forloop.sh
+++ forloop.sh
@@ -17,3 +17,9 @@ what is meant by ping
 > /dev/null: hide the output 
 2>: redirect standard output
 &1: redirect standard error 
+
+ping -c 1 google.com > /dev/null 2>&1	See nothing (everything hidden)
+ping -c 1 google.com	See full output and errors
+ping -c 1 google.com 2>&1 >/dev/null	See only errors
+ping -c 1 google.com > /dev/null 2> error.log	Output hidden, errors in error.log
+ping -c 1 google.com &> output.log	Both stdout and stderr in output.log

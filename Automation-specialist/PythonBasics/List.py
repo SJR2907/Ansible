ca9dae7e1612ea4f71d157726294810dda63e81a
Arunk2907 <arunlinuxdevops@gmail.com>
2025-05-18 18:50:13 +0530

    pythonscripts

---
+++ Automation-specialist/PythonBasics/List.py
@@ -0,0 +1,19 @@
+Ordered, mutable (can change), allows duplicates.
+# Create a List 
+fruits = ["apple","banana", "cherry"]
+
+# Access elements
+print(fruits[0] ) #Output: apple 
+
+# Modify element
+ fruits[1] = "blueberry"
+
+# Add element 
+  fruits.append("orange")
+
+# Remove element
+  fruits.remove("apple")
+
+# Loop through list 
+  for fruit in fruits:
+      print(fruit)
